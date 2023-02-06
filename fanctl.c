90d233d3e5625285575d2f69132c0e1189d4e598
MioNya4 <121242737+MioNya4@users.noreply.github.com>
2023-02-06 23:44:30 +0300

    Update fanctl.c

--- fanctl.c
+++ fanctl.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 
-int main(int argc, char **argv) {
-char a = '\x10';
-int x = 3 - (argv[1][0] - 49);
-printf("%d", x);
+int main() {
+char a;
+int x;
 FILE * ec_sys = fopen("/dev/ec", "r+");
+
 while (1)
-	{ fseek(ec_sys, 0x60, SEEK_SET);
-	  fwrite(&a, sizeof (char), 1, ec_sys);
-	  sleep(x); }
+  { ec_sys = fopen("/dev/ec", "r+");
+	fseek(ec_sys, 0x6A, SEEK_SET);
+	fread(&a, sizeof (char), 1, ec_sys);
+	fseek(ec_sys, 0x60, SEEK_SET);
+	fwrite(&a, sizeof (char), 1, ec_sys);
+	fflush(stdout);
+	if (a = 30) {sleep(3);};
+	if (a = 45) {sleep(2);};
+	if (a = 65) {sleep(1);}
+	else {sleep(10);};s
+	fclose(ec_sys); }
 fclose(ec_sys);
 }
